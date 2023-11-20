853280aba0b9403c8acd7ba7e8269719a92a89ac
zxmerge <zxmerge@gmail.com>
2023-11-21 01:35:51 +0800

    完善转换逻辑

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,13 +1,17 @@
 <script setup lang="ts">
-import type {
+import {
   ITable,
   IView,
   IField,
   IFieldMeta,
   ITableMeta,
-  ISingleSelectField,
+  INumberField,
   IDateTimeField,
   IViewMeta,
+  IOpenSegment,
+  FieldType,
+
+  ITextField,
 } from "@lark-base-open/js-sdk";
 import { bitable } from "@lark-base-open/js-sdk";
 import { BaseClient } from "@base-open/node-sdk";
@@ -19,20 +23,18 @@ import LogicBlock from "./LogicBlock.vue";
 
 const isTransforming = ref(false);
 const sucessCounter = ref(0);
+const failureCounter = ref(0);
 const stopFlag = ref(false);
 const finishFlag = ref(false);
 const recordCount = ref(0);
 
 const base = bitable.base;
-const tableNameList = ref<any[]>([]);
 const tableList = shallowRef<ITableMeta[]>([]);
 const activeTable = shallowRef<ITable>();
-const activeFiledList = ref<any>([]);
 const fieldList = shallowRef<IFieldMeta[]>([]);
 const activeTableName = ref("");
-const originField = ref();
-const targetFiled = ref();
-const activeView = shallowRef<IView | IViewMeta>();
+const originField = shallowRef<IFieldMeta>();
+const targetField = shallowRef<IFieldMeta>();
 const activeUnit = ref({
   label: "毫秒时间戳",
   value: "millis",
@@ -41,7 +43,7 @@ const activeTransformPattern = ref({
   label: "时间戳转日期",
   value: "timestampToDate",
 });
-const transformPatterList = [
+const transformPatternList = [
   {
     label: "时间戳转日期",
     value: "timestampToDate",
@@ -89,41 +91,113 @@ const getFieldList = async (table) => {
   return fieldList;
 };
 
-const handleTableSelect = async () => {
-  // const table = await base.getTableByName(activeTableName.value);
-  // const fieldList = await getFieldList(table);
-  // console.log(fieldList);
-  // activeFiledList.value = fieldList;
+const handleTableSelect = (): void => {
+  originField.value = undefined;
+  targetField.value = undefined;
+  // 增加
+};
+
+const handleReset = (): void => {
+  isTransforming.value = true;
+  stopFlag.value = false;
+  finishFlag.value = false;
+  sucessCounter.value = 0;
+  recordCount.value = 0;
+  failureCounter.value = 0;
+};
+
+const extractValueByRecordId = async (
+  singleSelectField: ITextField | IDateTimeField | INumberField,
+  recordId: string
+): Promise<number | null> => {
+  const originFieldVar = await singleSelectField.getValue(recordId);
+  if (originFieldVar === null) {
+    return null;
+  }
+  let res: number;
+  // 本身是 IDateTimeField || INumberField 类型
+  res = originFieldVar as number;
+  // 本身是 ITextField 类型
+  if (Array.isArray(originFieldVar) && originFieldVar[0].type == "text") {
+    res = Number(originFieldVar[0].text);
+  }
+  console.log("extractValueByRecordId Res: ", res);
+  return res;
+};
+
+const setValueByRecordId = async (
+  targetSelectField: ITextField | IDateTimeField | INumberField,
+  recordId: string,
+  targetVal: number
+): Promise<void> => {
+  let res: Promise<void>;
+  const field = targetSelectField as IDateTimeField | INumberField;
+  if (targetSelectField["IOpenSegment[]"]) {
+    console.log("文本");
+  } else {
+    console.log("非文本");
+  }
+  if (targetField.value?.type === FieldType.Text) {
+    res = (targetSelectField as ITextField)
+      .setValue(recordId, targetVal.toString())
+      .then(() => {
+        sucessCounter.value++;
+      })
+      .catch(() => {
+        failureCounter.value++;
+      });
+  } else {
+    res = (targetSelectField as IDateTimeField | INumberField)
+      .setValue(recordId, targetVal)
+      .then(() => {
+        sucessCounter.value++;
+      })
+      .catch(() => {
+        failureCounter.value++;
+      });
+  }
+
+  return res;
 };
 
 const handleConfirm = async () => {
-  isTransforming.value = true;
-  stopFlag.value = false;
-  sucessCounter.value = 0;
-  finishFlag.value = false;
-  recordCount.value = 0;
-  sucessCounter.value = 0;
-  console.log("originField: ", originField.value);
+  if (!(originField.value && targetField.value)) {
+    return;
+  }
+  handleReset();
   if (activeTable.value) {
+    console.log("开始1");
     const recordIdList = await activeTable.value.getRecordIdList();
-    const singleSelectField =
-      await activeTable.value.getField<ISingleSelectField>(originField.value);
-    const targetSelectField = await activeTable.value.getField<IDateTimeField>(
-      targetFiled.value
-    );
+    console.log("开始2");
+    const originSelectField = await activeTable.value.getField<
+      IDateTimeField | ITextField | INumberField
+    >(originField.value.id);
+    console.log("开始3");
+    const targetSelectField = await activeTable.value.getField<
+      IDateTimeField | ITextField
+    >(targetField.value.id);
     try {
       const promises: Promise<void>[] = [];
       for (const recordId of recordIdList) {
-        let val = await singleSelectField.getValue(recordId);
+        const val = await extractValueByRecordId(originSelectField, recordId);
+        if (!val) {
+          continue;
+        }
+        console.log(val);
         recordCount.value++;
-        if (val && !stopFlag.value) {
-          let targetVal = val[0].text;
-          const promise = targetSelectField
-            .setValue(recordId, Number(targetVal))
-            .then(() => {
-              sucessCounter.value++;
-            });
-          promises.push(promise);
+        if (!stopFlag.value) {
+          let targetVal: number;
+          if (activeTransformPattern.value.value == "timestampToDate") {
+            targetVal = activeUnit.value.value == `millis` ? val : val * 1000;
+          } else {
+            targetVal = activeUnit.value.value == `millis` ? val : val / 1000;
+          }
+          const proimse = setValueByRecordId(
+            targetSelectField,
+            recordId,
+            targetVal
+          );
+          promises.push(proimse);
         }
       }
       await Promise.all(promises);
@@ -133,8 +207,6 @@ const handleConfirm = async () => {
         console.log("all done");
       }
     }
-
-    
   }
 };
 
@@ -145,29 +217,28 @@ const handleStop = () => {
   stopFlag.value = true;
 };
 
-onMounted(async () => {
+const getCurrentSelection = async () => {
   const selection = await bitable.base.getSelection();
-  tableList.value = await bitable.base.getTableMetaList();
-  console.log("selection: ", selection);
   if (selection.tableId && selection.viewId) {
     activeTable.value = await bitable.base.getTableById(selection.tableId);
-    console.log("activeTable: ", activeTable);
     const view = await activeTable.value.getViewById(selection.viewId);
-    console.log("view: ", view);
     fieldList.value = await view.getFieldMetaList();
-    console.log("fieldList: ", fieldList.value);
   }
+};
 
-  // activeView.value = await activeTable.getActiveView();
-  const fieldLL = await activeView.value.getFieldMetaList();
-  console.log("activeView: ", fieldLL);
-  // activeTableName.value = await getTableName(activeTable);
-  // const tableList = await getTableList();
-  // const tempNameList: any[] = [];
-  // for (let i = 0; i < tableList.length; i++) {
-  //   tempNameList.push(await getTableName(tableList[i]));
-  // }
-  // tableNameList.value = tempNameList;
+const handleFieldChange = () => {
+  console.log("handleFieldChange");
+  getCurrentSelection();
+  originField.value = undefined;
+  targetField.value = undefined;
+}
+
+onMounted(async () => {
+  await getCurrentSelection();
+  // const off = bitable.base.onSelectionChange(handleSelectionChange);
+  const offFieldModify = activeTable.value?.onFieldModify(handleFieldChange);
+  const offFieldAdd = activeTable.value?.onFieldAdd(handleFieldChange);
+  const offFieldDelete= activeTable.value?.onFieldDelete(handleFieldChange);
 });
 </script>
 
@@ -193,13 +264,14 @@ onMounted(async () => {
       <el-form-item label="选择转换模式">
         <el-select
           v-model="activeTransformPattern"
+          value-key="value"
           class="m-2"
-          placeholder="请选择数据表"
+          placeholder="请选转换模式"
           :change="handleTableSelect()"
           style="width: 90%"
         >
           <el-option
-            v-for="item in transformPatterList"
+            v-for="item in transformPatternList"
             :key="item.value"
             :label="item.label"
             :value="item"
@@ -213,6 +285,7 @@ onMounted(async () => {
           >
             <el-select
               v-model="originField"
+              value-key="id"
               class="m-2"
               placeholder="选择时间戳字段"
               style="width: 60%; border-radius: 10px"
@@ -223,7 +296,7 @@ onMounted(async () => {
                 )"
                 :key="item.id"
                 :label="item.name"
-                :value="item.id"
+                :value="item"
               />
             </el-select>
             <el-select
@@ -243,7 +316,8 @@ onMounted(async () => {
         </el-form-item>
         <el-form-item label="选择生成目标字段（日期字段）">
           <el-select
-            v-model="targetFiled"
+            v-model="targetField"
+            value-key="id"
             class="m-2"
             placeholder="选择目标字段"
             style="width: 90%"
@@ -252,7 +326,7 @@ onMounted(async () => {
               v-for="item in fieldList.filter((item) => item.type === 5)"
               :key="item.id"
               :label="item.name"
-              :value="item.id"
+              :value="item"
             />
           </el-select>
         </el-form-item>
@@ -260,7 +334,8 @@ onMounted(async () => {
       <div v-else-if="activeTransformPattern.value == `dateToTimestamp`">
         <el-form-item label="选择源字段（日期字段）">
           <el-select
-            v-model="targetFiled"
+            v-model="originField"
+            value-key="id"
             class="m-2"
             placeholder="选择目标字段"
             style="width: 90%"
@@ -269,7 +344,7 @@ onMounted(async () => {
               v-for="item in fieldList.filter((item) => item.type === 5)"
               :key="item.id"
               :label="item.name"
-              :value="item.id"
+              :value="item"
             />
           </el-select>
         </el-form-item>
@@ -278,7 +353,8 @@ onMounted(async () => {
             style="width: 90%; display: flex; justify-content: space-between"
           >
             <el-select
-              v-model="originField"
+              v-model="targetField"
+              value-key="id"
               class="m-2"
               placeholder="选择时间戳字段"
               style="width: 60%; border-radius: 10px"
@@ -289,7 +365,7 @@ onMounted(async () => {
                 )"
                 :key="item.id"
                 :label="item.name"
-                :value="item.id"
+                :value="item"
               />
             </el-select>
             <el-select
@@ -316,19 +392,19 @@ onMounted(async () => {
       <el-button type="primary" @click="handleConfirm"> 开始转换 </el-button>
       <el-button @click="handleStop"> 停止 </el-button>
     </div>
-    <div
-      v-if="isTransforming"
-      style="padding-top: 20px; font-size: large; font-weight: medium"
-    >
-      <el-row style="display: flex; align-items: center; padding-inline;: 10px">
-        <span> 已转换： {{ sucessCounter }} </span>
-        <span v-if="finishFlag">
-          <el-tag v-if="!stopFlag" type="success" size="big"> 转换完成 </el-tag>
-        </span>
-        <span v-if="stopFlag">
-          <el-tag type="info" size="big"> 转换停止 </el-tag>
-        </span>
-      </el-row>
+    <div v-if="isTransforming" style="padding-top: 20px; font-size: medium">
+      <div style="padding-bottom: 10px">
+        <div style="padding-bottom: 10px">转换成功： {{ sucessCounter }}</div>
+        <div style="padding-bottom: 10px">转换失败： {{ failureCounter }}</div>
+        <div v-if="finishFlag" style="padding-bottom: 10px">
+          <el-tag v-if="!stopFlag" type="success" size="large">
+            转换完成
+          </el-tag>
+        </div>
+        <div v-if="stopFlag" style="padding-bottom: 10px">
+          <el-tag type="info" size="large"> 转换停止 </el-tag>
+        </div>
+      </div>
     </div>
   </div>
 </template>
